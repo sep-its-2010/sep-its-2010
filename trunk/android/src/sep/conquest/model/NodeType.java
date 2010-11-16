64c1fa0aee7f0565bae56b4735c75a10bab6c2cb
lorenz.florian <lorenz.florian@84c54321-3497-816b-81ed-ece8ebfe0662>
2010-11-16 15:56:00 +0000

    NodeType Kommentare ausgeführt und geupdatet

--- trunk/android/src/sep/conquest/model/NodeType.java
+++ trunk/android/src/sep/conquest/model/NodeType.java
@@ -6,14 +6,53 @@ package sep.conquest.model;
  * @author Florian Lorenz
  */
 public enum NodeType{
+	/**
+	 * Left upper corner
+	 */
 	TOPLEFTEDGE,
+	
+	/**
+	 * Right upper corner
+	 */
 	TOPRIGHTEDGE,
+	
+	/**
+	 * Left lower corner
+	 */
 	BOTTOMLEFTEDGE,
+	
+	/**
+	 * Right lower corner
+	 */
 	BOTTOMRIGHTEDGE,
+	
+	/**
+	 * T-Cross
+	 */
 	TOPT,
+	
+	/**
+	 * T-Cross turned 180 degrees
+	 */
 	BOTTOMT,
+	
+	/**
+	 * T-Cross turned 270 degrees
+	 */
 	LEFTT,
+	
+	/**
+	 * T-Cross turned 90 degrees
+	 */
 	RIGHTT,
+
+	/**
+	 * Two-line cross
+	 */
 	CROSS,
+	
+	/**
+	 * The node hasen't been visited yet
+	 */
 	FRONTIERNODE;
 }
