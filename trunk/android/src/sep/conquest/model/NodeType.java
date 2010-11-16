166bdb652542a0c3dd84379c143f2f2c90acee00
lorenz.florian <lorenz.florian@84c54321-3497-816b-81ed-ece8ebfe0662>
2010-11-16 15:39:11 +0000

    Erste Version vom enum NodeType hochgeladen

---
+++ trunk/android/src/sep/conquest/model/NodeType.java
@@ -0,0 +1,19 @@
+package sep.conquest.model;
+
+/**
+ * The enum NodeType saves the
+ * 
+ * @author Florian Lorenz
+ */
+public enum NodeType{
+	TOPLEFTEDGE,
+	TOPRIGHTEDGE,
+	BOTTOMLEFTEDGE,
+	BOTTOMRIGHTEDGE,
+	TOPT,
+	BOTTOMT,
+	LEFTT,
+	RIGHTT,
+	CROSS,
+	FRONTIERNODE;
+}
