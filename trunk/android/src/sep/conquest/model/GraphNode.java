c6dde04ba8f581feb3fb0d2f9bac54132e3ce7da
lorenz.florian <lorenz.florian@84c54321-3497-816b-81ed-ece8ebfe0662>
2010-11-16 15:38:48 +0000

    Erste Version von Graphnode hochgeladen

---
+++ trunk/android/src/sep/conquest/model/GraphNode.java
@@ -0,0 +1,23 @@
+package sep.conquest.model;
+
+/**
+ * The class GraphNode inherits of the class MapNode and saves additionally
+ * the references to its neighbours. This is necessary for the A*-Algorithm
+ * 
+ * @author Florian Lorenz
+ */
+public class GraphNode extends MapNode {
+	
+	/**
+	 * Saves the references on the direct neighbours of the node
+	 */
+	private MapNode[] neighbours = new MapNode[4];
+	
+	/**
+	 * This method returns a reference on the direct neighbours of the node
+	 * @return the attribute neighbours
+	 */
+	public MapNode[] getNeighbours(){
+		
+	}
+}
