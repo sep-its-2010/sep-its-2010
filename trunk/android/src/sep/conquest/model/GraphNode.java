c5e28469146430748c22d2ce5c774cc96f9e1e58
lorenz.florian <lorenz.florian@84c54321-3497-816b-81ed-ece8ebfe0662>
2010-11-17 09:18:49 +0000

    Konstanten hinzugefügt für den Zugriff auf das array das die nachbarn speichert (inkl comments)

--- trunk/android/src/sep/conquest/model/GraphNode.java
+++ trunk/android/src/sep/conquest/model/GraphNode.java
@@ -2,11 +2,36 @@ package sep.conquest.model;
 
 /**
  * The class GraphNode inherits of the class MapNode and saves additionally
- * the references to its neighbours. This is necessary for the A*-Algorithm
+ * the references to its neighbours in an array. This is necessary for the 
+ * A*-Algorithm
  * 
  * @author Florian Lorenz
  */
 public class GraphNode extends MapNode {
+	
+	/**
+	 * This constant exists for the access to the attribute neighbours. Position
+	 * 0 represents the left neighbour
+	 */
+	public final static int LEFTNEIGHBOUR = 0;
+	
+	/**
+	 * This constant exists for the access to the attribute neighbours. Position
+	 * 1 represents the right neighbour
+	 */
+	public final static int RIGHTNEIGHBOUR = 1;
+	
+	/**
+	 * This constant exists for the access to the attribute neighbours. Position
+	 * 2 represents the lower neighbour
+	 */
+	public final static int BOTTOMNEIGHBOUR = 2;
+	
+	/**
+	 * This constant exists for the access to the attribute neighbours. Position
+	 * 3 represents the upper neighbour
+	 */
+	public final static int TOPNEIGHBOUR = 3;
 	
 	/**
 	 * Saves the references on the direct neighbours of the node
