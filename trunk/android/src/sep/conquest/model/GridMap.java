05dac57d1ecd3839638f1bb9d8d9fdea8f648293
lorenz.florian <lorenz.florian@84c54321-3497-816b-81ed-ece8ebfe0662>
2010-11-18 15:12:23 +0000

    GridMap erweitert, speichert jetzt die max und min x und y koordinaten. Wird fuer kartenzeichnung von flo gebraucht, kommentare vervollstaendigt. Den Muell liest sowieso keiner, deswegen hier ein kleiner Test.. frohe weihnachten euch allen... :-D

--- trunk/android/src/sep/conquest/model/GridMap.java
+++ trunk/android/src/sep/conquest/model/GridMap.java
@@ -6,11 +6,11 @@ import java.util.TreeMap;
 
 /**
  * The class GridMap manages the saving of nodes which have been explored by the
- * e-puck roboter. Nodes of the type GraphNode can be added to the structure and the
- * access is managed with a binary tree. Additionally the class GridMap has got a
- * attribute frontierList which saves the unexplored nodes with explored
- * neighbours. GridMap implements the interface Serializable to serialize the data
- * structure.
+ * e-puck roboter. Nodes of the type GraphNode can be added to the structure and 
+ * the access is managed with a binary tree. Additionally the class GridMap has
+ * got a attribute frontierList which saves the unexplored nodes with explored
+ * neighbours. GridMap implements the interface Serializable to serialize the 
+ * data structure.
  * 
  * @author Florian Lorenz
  */
@@ -23,16 +23,48 @@ public class GridMap implements Serializable{
 
 	/**
 	 * Saves all instances of the class MapNode, which haven't been explored
-	 * yet, but which has got at least one visited neighbour of the type GraphNode
+	 * yet, but which has got at least one visited neighbour of the type 
+	 * GraphNode
 	 */
-	private LinkedList<GraphNode> frontierList = new LinkedList<GraphNode>();;
+	private LinkedList<GraphNode> frontierList = new LinkedList<GraphNode>();
 
 	/**
-	 * Saves all references of the instances of the class GraphNode, ordered in a
-	 * binary tree.
+	 * Saves all references of the instances of the class GraphNode, ordered in 
+	 * a binary tree.
 	 */
-	private TreeMap<Integer, GraphNode> mapTree = new TreeMap<Integer, GraphNode>();
-
+	private TreeMap<Integer, GraphNode> mapTree = new TreeMap<Integer, 
+																GraphNode>();
+	
+	/**
+	 * Saves the smallest and biggest x- and y-coordinates of the MapNodes in 
+	 * the class GridMap. 
+	 */
+	private int[] mapBorders = new int[4];
+	
+	/**
+	 * This constant represents the smallest x-coordinate of all MapNodes saved
+	 * in the attribute mapBorder on position 0.
+	 */
+	private final static int minXValueOnMap = 0;
+	
+	/**
+	 * This constant represents the smallest y-coordinate of all MapNodes saved
+	 * in the attribute mapBorder on position 1.
+	 */
+	private final static int minYValueOnMap = 1;
+	
+	/**
+	 * This constant represents the biggest x-coordinate of all MapNodes saved
+	 * in the attribute mapBorder on position 2.
+	 */
+	private final static int maxXValueOnMap = 2;
+	
+	/**
+	 * This constant represents the biggest y-coordinate of all MapNodes saved
+	 * in the attribute mapBorder on position 3.
+	 */
+	private final static int maxYValueOnMap = 3;
+	
 	/**
 	 * default constructor
 	 */
@@ -60,9 +92,9 @@ public class GridMap implements Serializable{
 	}
 
 	/**
-	 * Makes a copy of the attribute mapTree using the private method cloneMapTreeIntoList
-	 * and saves the nodes of the tree as instances of the class MapNodes 
-	 * in a LinkedList
+	 * Makes a copy of the attribute mapTree using the private method 
+	 * cloneMapTreeIntoList and saves the nodes of the tree as instances of the
+	 * class MapNodes in a LinkedList
 	 * 
 	 * @return Returns a LinkedList which contains all Nodes saved in the
 	 *         GridMap
@@ -73,7 +105,28 @@ public class GridMap implements Serializable{
 	}
 
 	/**
-	 * Makes a deep copy of the frontierList and saves the GraphNodes in a LinkedList
+	 * This method returns the smallest and biggest x- and y-coordinates of
+	 * the map saved in an int-array of length 4.
+	 * @return Returns the attribute mapBorders which contains the smallest and
+	 * biggest coordinates. 
+	 */
+	public int[] getMapBorders(){
+		return this.mapBorders;
+	}
+	
+	/**
+	 * This method is called by the public method addNode and updates the 
+	 * smallest or biggest coordinates if necessary.
+	 * @param xCoordinate Is the x-coordinate of the new inserted MapNode
+	 * @param yCoordinate Is the y-coordinate of the new inserted MapNode
+	 */
+	private void updateMapBorders(int xCoordinate, int yCoordinate){
+		
+	}
+	
+	/**
+	 * Makes a deep copy of the frontierList and saves the GraphNodes in a 
+	 * LinkedList
 	 * 
 	 * @return Returns a LinkedList which is a copy of frontierList
 	 */
@@ -83,8 +136,8 @@ public class GridMap implements Serializable{
 	}
 
 	/**
-	 * Iterates trough the TreeMap mapTree, creates MapNodes using the GraphNodes
-	 * and saves them into a LinkedList
+	 * Iterates trough the TreeMap mapTree, creates MapNodes using the 
+	 * GraphNodes and saves them into a LinkedList
 	 * 
 	 * @return Return a LinkedList which contains all nodes saved in the GridMap
 	 */
@@ -93,8 +146,8 @@ public class GridMap implements Serializable{
 	}
 
 	/**
-	 * Searches whether a node with the coordinates x and y exists in the TreeMap
-	 * mapTree
+	 * Searches whether a node with the coordinates x and y exists in the 
+	 * TreeMap mapTree
 	 * 
 	 * @param x Is the x-coordinate 
 	 * @param y Is the y-coordinate          
