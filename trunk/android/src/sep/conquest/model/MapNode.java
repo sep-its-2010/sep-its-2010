86e5e893bb58f0dfc96277df64e3fc0fccb04590
lorenz.florian <lorenz.florian@84c54321-3497-816b-81ed-ece8ebfe0662>
2010-11-18 13:53:17 +0000

    Klasse MapNode serializable gemacht, Kommentare verbessert, Konstruktor ueberarbeitet..

--- trunk/android/src/sep/conquest/model/MapNode.java
+++ trunk/android/src/sep/conquest/model/MapNode.java
@@ -1,13 +1,20 @@
 package sep.conquest.model;
 
+import java.io.Serializable;
+
 /**
  * The class MapNode represents a node on the pitch. It saves the coordinates, the type
  * of the node and how often an e-puck roboter visited the node.
  * 
  * @author Florian Lorenz
  */
-public class MapNode {
+public class MapNode implements Serializable {
 	
+	/**
+	 * An unique serialVersionUID to identify the class
+	 */
+	private static final long serialVersionUID = 8344046024055788574L;
+
 	/**
 	 * Saves the x-coordinate of the node
 	 */
@@ -29,66 +36,72 @@ public class MapNode {
 	private NodeType nodeState;
 	
 	/**
-	 * default constructor
+	 * Constructor creates an instance of the class MapNode and sets the
+	 * coordinates and the NodeType
+	 * @param newXValue Is the x-coordinate of the new MapNode
+	 * @param newYValue Is the y-coordinate of the new MapNode
+	 * @param newnodeState Is the NodeType of the new MapNode
 	 */
-	public MapNode(){
-		
+	public MapNode(int newXValue, int newYValue,NodeType newNodeState){
+		this.xValue = newXValue;
+		this.yValue = newYValue;
+		this.nodeState = newNodeState;
 	}
 	
 	/**
 	 * This method returns the x-coordinate of the node
-	 * @return returns the attribute xValue
+	 * @return Returns the attribute xValue
 	 */
 	public int getXValue(){
-	  return 0;
+	  return this.xValue;
 	}
 	
 	/**
 	 * This method returns the y-coordinate of the node
-	 * @return returns the attribute yValue
+	 * @return Returns the attribute yValue
 	 */
 	public int getYValue(){
-	  return 0;	
+	  return this.yValue;	
 	}
 	
 	/**
 	 * This method returns the type of the node
-	 * @return returns the attribute nodeType
+	 * @return Returns the attribute nodeType
 	 */
 	public NodeType getNodeType(){
-	  return null;
+	  return this.nodeState;
 	}
 	
 	/**
 	 * This method returns how often an e-puck visited the node
-	 * @return returns the attribute  
+	 * @return Returns the attribute  
 	 */
 	public int getVisitCounter(){
-	  return 0;
+	  return this.visitCounter;
 	}
 	
 	/**
 	 * This method changes the x-coordinate of the node
-	 * @param newXValue is the new x-coordinate
+	 * @param newXValue Is the new x-coordinate
 	 */
 	public void setXValue(int newXValue){
-		
+		this.xValue = newXValue;
 	}
 	
 	/**
 	 * This method changes the y-coordinate of the node
-	 * @param newYValue is the new y-coordinate
+	 * @param newYValue Is the new y-coordinate
 	 */
 	public void setYValue(int newYValue){
-		
+		this.yValue = newYValue;
 	}
 	
 	/**
 	 * This method changes the type of the node
-	 * @param newNodeType is the new nodeType
+	 * @param newNodeType Is the new nodeType
 	 */
-	public void setNodeType(NodeType newNodeType){
-		
+	public void setNodeType(NodeType newNodeState){
+		this.nodeState = newNodeState;
 	}
 	
 	/**
@@ -96,6 +109,6 @@ public class MapNode {
 	 * attribut visitCounter
 	 */
 	public void increaseVisitCounter(){
-		
+		this.visitCounter++;
 	}
 }
