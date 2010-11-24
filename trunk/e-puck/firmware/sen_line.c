f3ceac91883ff5bc0bce5e86deea277c21567693
maxbinder007@gmx.de <maxbinder007@gmx.de@84c54321-3497-816b-81ed-ece8ebfe0662>
2010-11-24 10:56:17 +0000

    fast fertig implementiert

--- trunk/e-puck/firmware/sen_line.c
+++ trunk/e-puck/firmware/sen_line.c
@@ -1,9 +1,10 @@
 #include "sen_line.h"
+#include "hal_i2c.h"
 
 enum {
-	SENSOR_I2C_Address = 0xC0;			///< adress of ground-sensors on the I2C-BUS
+	SENSOR_I2C_ADDRESS = 0xC0		///< adress of ground-sensors on the I2C-BUS
 	// könnte auch 0xD4 sein, muss noch herausgefunden werden
-}
+};
 
 /*!
  * \brief
@@ -23,21 +24,9 @@ enum {
 void sen_line_read(
 	OUT sen_line_SData_t* const _lppodData
 	) {
-		uint8_t aui8RawBuffer[30] = {0};
-		uint16_t aui16Buffer[3] = {0};			///< stores sensordata temporarily
-
-        for( uint8_t j = 0; j < 6; j++) {
-			if( j % 2 == 0) {
-				hal_i2c_read( SENSOR_I2C_Address, aui8RawBuffer[j + 1], 1);
-				// TODO letzter Parameter der read-Funktion ist mir unklar. Im Demo wird byteweise über den BUS ausgelesen und anschließend konkateniert, geht das bei uns vielleicht sogar bequemer?
-				// muss ich hal_i2c_read einen pointer auf das array übergeben?
-			}
-			else if( j % 2 != 0) {
-				hal_i2c_read(SENSOR_I2C_Address, aui8RawBuffer[j - 1], 1);
-			}
+		// reads 2 Bytes of data from each ground-sensor
+        for( uint8_t i = 0; i < 3; i++) {
+			hal_i2c_read( SENSOR_I2C_ADDRESS, _lppodData, 2);
+			// TODO Reihenfolge der Bytes überprüfen
         }
-		aui16Buffer[0] = (unsigned int) (aui8RawBuffer[0] << 8) | ((unsigned int) aui8RawBuffer[1]);
-		aui16Buffer[1] = (unsigned int) (aui8RawBuffer[2] << 8) | ((unsigned int) aui8RawBuffer[3]);
-		aui16Buffer[2] = (unsigned int) (aui8RawBuffer[4] << 8) | ((unsigned int) aui8RawBuffer[5]);
-		// TODO Nachfragen ob diese Konkatenation der Bytes so funktionieren kann
 }
