e09de2ed180ea7a194b27de1081e230b0d78678e
lorenz.florian@gmail.com <lorenz.florian@gmail.com@84c54321-3497-816b-81ed-ece8ebfe0662>
2010-11-29 11:52:48 +0000

    FailureType geupadtet

--- trunk/android/src/sep/conquest/model/FailureType.java
+++ trunk/android/src/sep/conquest/model/FailureType.java
@@ -8,7 +8,24 @@ package sep.conquest.model;
  *
  */
 public enum FailureType {
+	//SpielFeld ungueltig (Abgrund)
+	//SpielFeld ungueltig (Knoten falsch)
+	//Bluetoothausfall
+	//KLeine mögliche Kollisionsverwaltung
+	//Keine Möglichkeit den nächsten Knoten anzufahren
+	/**
+	 * Invalid playing field
+	 */
+	INVALIDPITCH,
 	
+	/**
+	 * If the bluetooth breaks down
+	 */
+	BLUETOOTHFAILURE,
 	
+	/**
+	 * If a node cannot be visited, because it's obstructed
+	 */
+	NODEOBSTRUCTION;
 
 }
