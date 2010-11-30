20997d95b7b87a68a5b9e0ab4e1bdc206ee9a867
lorenz.florian@gmail.com <lorenz.florian@gmail.com@84c54321-3497-816b-81ed-ece8ebfe0662>
2010-11-30 15:49:26 +0000

    PuckRequest geschrieben.. Und da das wieder keiner von euch liest wuensch ich euch diesmal sogar Frohe Ostern und ein gutes neues Jahr

--- trunk/android/src/sep/conquest/model/requests/PuckRequest.java
+++ trunk/android/src/sep/conquest/model/requests/PuckRequest.java
@@ -22,6 +22,61 @@ public class PuckRequest implements IRequest {
     
     private MessageType type;
     
+    /*
+     * Represents the byte-Code for the messageType Request_Reset
+     */
+    private static final short reqReset = 0x01FF;
+    
+    /*
+     * Represents the byte-Code for the messageType Request_Status
+     */
+    private static final short reqStatus = 0x02FF;
+    
+    /*
+     * Represents the byte-Code for the messageType Request_Turn
+     */
+    private static final short reqTurn = 0x03FF;
+    
+    /*
+     * Represents the byte-Code for the messageType Request_Move
+     */
+    private static final short reqMove = 0x04FF;
+    
+    /*
+     * Represents the byte-Code for the messageType Request_SetSpeed
+     */
+    private static final short reqSetSpeed = 0x05FF;
+    
+    /*
+     * Represents the byte-Code for the messageType Request_SetLed
+     */
+    private static final short reqSetLed = 0x06FF;
+    
+    /*
+     * Represents the byte-Code for the messageType Response_Ok
+     */
+    private static final short resOk = (short) 0x81FF;
+    
+    /*
+     * Represents the byte-Code for the messageType Response_Status
+     */
+    private static final short resStatus = (short) 0x82FF;
+    
+    /*
+     * Represents the byte-Code for the messageType Response_HitNode
+     */
+    private static final short resHitNode = (short) 0x83FF;
+    
+    /*
+     * Represents the byte-Code for the messageType Response_Collision
+     */
+    private static final short resCollision = (short) 0x84FF;
+    
+    /*
+     * Represents the byte-Code for the messageType Response_Abyss
+     */
+    private static final short resAbyss = (short) 0x85FF;
+    
     /**
      * The constructor expects an request message to identify the type of the
      * message as well as its corresponding data. It stores the specific
@@ -30,8 +85,35 @@ public class PuckRequest implements IRequest {
      * @param message The input-message to determine the type and data.
      */
     public PuckRequest(byte[] message) {
-    	message = this.message;
-        type = MessageType.PUCK_OK; // TODO determine correct message-type
+    	for (int i = 0; i < 32; i++) {
+			this.message[i] = message[i];
+		}
+		short messageType;
+		messageType = (short) (((message[1] & 0xFF) << 8) | message[0] & 0xFF);
+		switch (messageType) {
+		case resOk:
+			type = MessageType.RESPONSE_OK;
+		case resAbyss:
+			type = MessageType.RESPONSE_ABYSS;
+		case resCollision:
+			type = MessageType.RESPONSE_COLLISION;
+		case resHitNode:
+			type = MessageType.RESPONSE_HIT_NODE;
+		case resStatus:
+			type = MessageType.RESPONSE_STATUS;
+		case reqMove:
+			type = MessageType.REQUEST_MOVE;
+		case reqReset:
+			type = MessageType.REQUEST_RESET;
+		case reqSetLed:
+			type = MessageType.REQUEST_SET_LED;
+		case reqSetSpeed:
+			type = MessageType.REQUEST_SET_SPEED;
+		case reqStatus:
+			type = MessageType.REQUEST_STATUS;
+		case reqTurn:
+			type = MessageType.REQUEST_TURN;
+		}	
     }
 
     /* (non-Javadoc)
