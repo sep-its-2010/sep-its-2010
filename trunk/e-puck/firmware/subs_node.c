f8022c403140139a1560572acf795cd7b6a390a1
maxbinder007@gmx.de <maxbinder007@gmx.de@84c54321-3497-816b-81ed-ece8ebfe0662>
2010-12-06 11:15:34 +0000

    refactoring der Knotenerkennung

--- trunk/e-puck/firmware/subs_node.c
+++ trunk/e-puck/firmware/subs_node.c
@@ -15,10 +15,13 @@ enum {
 enum {
 	NODE_TYPE__TOP_LEFT_EDGE = 0,
 	NODE_TYPE__TOP_RIGHT_EDGE = 1,
-	NODE_TYPE__TOP_T = 2,
-	NODE_TYPE__RIGHT_T = 3,
-	NODE_TYPE__LEFT_T = 4,
-	NODE_TYPE__CROSS = 5
+	NODE_TYPE__BOTTOM_LEFT_EDGE = 2,
+	NODE_TYPE__BOTTOM_RIGHT_EDGE = 3,
+	NODE_TYPE__TOP_T = 4,
+	NODE_TYPE__RIGHT_T = 5,
+	NODE_TYPE__BOTTOM_T = 6,
+	NODE_TYPE__LEFT_T = 7,
+	NODE_TYPE__CROSS = 8
 };
 
 uint8_t ui8NodeDetectionCounter = 0; ///< Number of ground-sensor-measurements in a row, which provided data below a certain threshold.
@@ -39,31 +42,31 @@ uint16_t ui16AvgRight = 0; ///< Stores values of the right ground-sensor, which 
  * The robot will stop with its center above the node and visualize its state.
  */
 bool subs_node_run( void) {
-	bool nodeHit = false;
-	
+	bool nodeHit = false;	
 	sen_line_read( &podSensorData);
 
 	// node detection-measurement
 	// TODO sollte man diese Multiplikation mit 2 in einer Konstante ablegen und besser dokumentieren?
  	if ((2 * (podSensorData.aui16Data[0]) < 1) || // 1 wird ersetzt durch EEPROM-Kalibrierwert für linken Sensor über Linie
- 		(2 * (podSensorData.aui16Data[2]) < 1)) { // 1 wird ersetzt durch EEPROM-Kalibrierwert für rechten Sensor über Linie		
-		
-		if (ui8NodeDetectionCounter == 0) {
-			hal_motors_setSteps(0);
-			ui16AvgLeft = 0;
-			ui16AvgRight = 0;
-		}
+ 		(2 * (podSensorData.aui16Data[2]) < 1)) { // 1 wird ersetzt durch EEPROM-Kalibrierwert für rechten Sensor über Linie
 		ui16AvgLeft += podSensorData.aui16Data[0];
 		ui16AvgRight += podSensorData.aui16Data[2];
-		ui8NodeDetectionCounter += 1;
-	}	
-	bool hasMoved = (hal_motors_getStepsLeft() >= 240) && (hal_motors_getStepsRight() >= 240);
+		ui8NodeDetectionCounter++;
+	} else {
+		hal_motors_setSteps(0);
+		ui16AvgLeft = 0;
+		ui16AvgRight = 0;
+		ui8NodeDetectionCounter = 0;
+	}
+	bool hasMoved = (hal_motors_getStepsLeft() > 0) && (hal_motors_getStepsRight() > 0);
 	
 	// robot is above a node
 	if( (ui8NodeDetectionCounter >= NODE_DETECTION__REQUIRED_MEASUREMENTS) && hasMoved) {
+		hal_motors_setSpeed( 0, 0); // @TODO hier muss man eventuell noch ein bisschen fahren, so dass der e-puck genau über dem Knoten steht
+		hal_motors_setSteps( 0);
+		nodeHit = true;		
 		ui16AvgLeft = ui16AvgLeft / ui8NodeDetectionCounter;
-		ui16AvgRight = ui16AvgRight / ui8NodeDetectionCounter;
-		nodeHit = true;
+		ui16AvgRight = ui16AvgRight / ui8NodeDetectionCounter;		
 
 		// analyze the shape of the node
 		uint8_t ui8NodeType;
@@ -83,8 +86,8 @@ bool subs_node_run( void) {
 		}
 		ui16AvgLeft = 0;
 		ui16AvgRight = 0;
-		hal_motors_setSpeed( 0, 0); // @TODO hier muss man eventuell noch ein bisschen fahren, so dass der e-puck genau über dem Knoten steht
-		hal_motors_setSteps( 0);
+		
+		// inform smartphone about node-detection
 		com_SMessage_t podHitNodeMessage = { COM_MESSAGE_TYPE__RESPONSE_HIT_NODE, {0}};
 		podHitNodeMessage.aui8Data[0] = ui8NodeType;
 		com_send( &podHitNodeMessage);
