c935be6a83cf5c10b4eee2203a15f04bb42f699e
andreas.poxrucker@gmx.de <andreas.poxrucker@gmx.de@84c54321-3497-816b-81ed-ece8ebfe0662>
2010-12-07 15:20:27 +0000

    Import mit Map als Parcel

--- trunk/android/src/sep/conquest/activity/Import.java
+++ trunk/android/src/sep/conquest/activity/Import.java
@@ -15,6 +15,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.os.Parcel;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
