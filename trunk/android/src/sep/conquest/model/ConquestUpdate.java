08807d551b869263717196b906fc4a8017497db6
andreas.poxrucker@gmx.de <andreas.poxrucker@gmx.de@84c54321-3497-816b-81ed-ece8ebfe0662>
2010-12-21 09:30:10 +0000

    Clone Methode klont jetzt auch die MapList

--- trunk/android/src/sep/conquest/model/ConquestUpdate.java
+++ trunk/android/src/sep/conquest/model/ConquestUpdate.java
@@ -16,135 +16,148 @@ import java.util.UUID;
  */
 public class ConquestUpdate implements Cloneable {
 
-	/**
-	 * The list of map-nodes of the global map.
-	 */
-	private List<MapNode> mapList;
+  /**
+   * The list of map-nodes of the global map.
+   */
+  private List<MapNode> mapList;
 
-	/**
-	 * The status of every robot on the map.
-	 */
-	private Map<UUID, RobotStatus> robots;
-	
-	/**
-	 * The names of the robots.
-	 */
-	private Map<UUID, String> robotNames = new TreeMap<UUID, String>();
-	
-	/**
-	 * The borders of the map.
-	 */
-	private int[] borders;
+  /**
+   * The status of every robot on the map.
+   */
+  private Map<UUID, RobotStatus> robots;
 
-	/**
-	 * The constructor expects a list of map-nodes as well as the status of
-	 * every robot on the map.
-	 * 
-	 * @param mapList
-	 *            The list of map-nodes.
-	 * @param robots
-	 *            The status of the robots, which are distinguished by the id.
-	 */
-	public ConquestUpdate(List<MapNode> mapList, int[] borders,
-			Map<UUID, RobotStatus> robots) {
-		this.mapList = mapList;
-		this.robots = robots;
-		this.borders = borders;
-		ComManager comMan = ComManager.getInstance();
-		for (UUID key: robots.keySet()) {
-			Puck robot = (Puck) comMan.getClient(key);
-			robotNames.put(key, robot.getName());
-		}
-	}
+  /**
+   * The names of the robots.
+   */
+  private Map<UUID, String> robotNames = new TreeMap<UUID, String>();
 
-	/**
-	 * Sets a new status for a specific (id) robot.
-	 * 
-	 * @param id The id of the robot.
-	 * @param status The status of the robot.
-	 */
-	public void setRobotStatus(UUID id, RobotStatus status) {
-		if (robots.containsKey(id))
-			robots.get(id).setRobotStatus(status);
-		else
-			robots.put(id, status);
-	}
+  /**
+   * The borders of the map.
+   */
+  private int[] borders;
 
-	/**
-	 * Sets a new list of map-nodes for the update.
-	 * 
-	 * @param mapList
-	 */
-	public void setMapList(LinkedList<MapNode> mapList) {
-		this.mapList = mapList;
-	}
+  /**
+   * The constructor expects a list of map-nodes as well as the status of every
+   * robot on the map.
+   * 
+   * @param mapList
+   *          The list of map-nodes.
+   * @param robots
+   *          The status of the robots, which are distinguished by the id.
+   */
+  public ConquestUpdate(List<MapNode> mapList, int[] borders,
+      Map<UUID, RobotStatus> robots) {
+    this.mapList = mapList;
+    this.robots = robots;
+    this.borders = borders;
+    ComManager comMan = ComManager.getInstance();
+    for (UUID key : robots.keySet()) {
+      Puck robot = (Puck) comMan.getClient(key);
+      robotNames.put(key, robot.getName());
+    }
+  }
 
-	/**
-	 * Returns the list of map-nodes.
-	 * 
-	 * @return The map-nodes.
-	 */
-	public List<MapNode> getMapList() {
-		return mapList;
-	}
+  /**
+   * Sets a new status for a specific (id) robot.
+   * 
+   * @param id
+   *          The id of the robot.
+   * @param status
+   *          The status of the robot.
+   */
+  public void setRobotStatus(UUID id, RobotStatus status) {
+    if (robots.containsKey(id))
+      robots.get(id).setRobotStatus(status);
+    else
+      robots.put(id, status);
+  }
 
-	/**
-	 * Returns the status of every robot in a map
-	 * 
-	 * @return The map of robot-status.
-	 */
-	public Map<UUID, RobotStatus> getRobotStatus() {
-		return robots;
-	}
+  /**
+   * Sets a new list of map-nodes for the update.
+   * 
+   * @param mapList
+   */
+  public void setMapList(LinkedList<MapNode> mapList) {
+    this.mapList = mapList;
+  }
 
-	/**
-	 * Returns a single status of a specific robot.
-	 * 
-	 * @param id The id of the robot.
-	 * @return The status of the robot.
-	 */
-	public RobotStatus getRobotStatus(UUID id) {
-		return robots.get(id);
-	}
-	
-	/**
-	 * Sets new borders.
-	 * 
-	 * @param borders The Borders.
-	 */
-	public void setBorders(int[] borders) {
-		this.borders = borders;
-	}
-	
-	/**
-	 * Returns the name of a specific robot if available. Otherwise it returns
-	 * null. 
-	 * 
-	 * @param id The id of the robot.
-	 * @return The name of the robot.
-	 */
-	public String getRobotName(UUID id) {
-		return robotNames.get(id);
-	}
-	
-	/**
-	 * Returns the borders.
-	 * 
-	 * @return The borders.
-	 */
-	public int[] getBorders() {
-		return borders;
-	}
-	
-	/* (non-Javadoc)
-	 * @see java.lang.Object#clone()
-	 */
-	@Override public ConquestUpdate clone() {
-		Map<UUID, RobotStatus> copyMap = new TreeMap<UUID, RobotStatus>();
-		Set<UUID> keys = robots.keySet();
-		for (UUID key: keys) {
-			copyMap.put(key, robots.get(key).clone());
-		}		
-		return new ConquestUpdate(mapList, borders, copyMap);
-	}
+  /**
+   * Returns the list of map-nodes.
+   * 
+   * @return The map-nodes.
+   */
+  public List<MapNode> getMapList() {
+    return mapList;
+  }
+
+  /**
+   * Returns the status of every robot in a map
+   * 
+   * @return The map of robot-status.
+   */
+  public Map<UUID, RobotStatus> getRobotStatus() {
+    return robots;
+  }
+
+  /**
+   * Returns a single status of a specific robot.
+   * 
+   * @param id
+   *          The id of the robot.
+   * @return The status of the robot.
+   */
+  public RobotStatus getRobotStatus(UUID id) {
+    return robots.get(id);
+  }
+
+  /**
+   * Sets new borders.
+   * 
+   * @param borders
+   *          The Borders.
+   */
+  public void setBorders(int[] borders) {
+    this.borders = borders;
+  }
+
+  /**
+   * Returns the name of a specific robot if available. Otherwise it returns
+   * null.
+   * 
+   * @param id
+   *          The id of the robot.
+   * @return The name of the robot.
+   */
+  public String getRobotName(UUID id) {
+    return robotNames.get(id);
+  }
+
+  /**
+   * Returns the borders.
+   * 
+   * @return The borders.
+   */
+  public int[] getBorders() {
+    return borders;
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see java.lang.Object#clone()
+   */
+  @Override
+  public ConquestUpdate clone() {
+    Map<UUID, RobotStatus> copyMap = new TreeMap<UUID, RobotStatus>();
+    Set<UUID> keys = robots.keySet();
+    for (UUID key : keys) {
+      copyMap.put(key, robots.get(key).clone());
+    }
+    
+    LinkedList<MapNode> copyList = new LinkedList<MapNode>();
+    for (MapNode node : mapList) {
+      copyList.add(node);
+    }
+    return new ConquestUpdate(copyList, borders, copyMap);
+  }
 }
